27d9b347ab9466d4c39a77c006a84931e6265cbd
aaravdubey <daarav101@gmail.com>
2023-08-17 22:21:39 +0530

    ui improved

---
+++ contactMe.js
@@ -0,0 +1,15 @@
+// function handleSubmit() {
+//   alert('hhh')
+//   Email.send({
+//       Host: "smtp.gmail.com",
+//       Username: "daarav101@gmail.com",
+//       Password: "aarav30112002",
+//       To: 'daarav101@gmail.com',
+//       From: "daarav101@gmail.com",
+//       Subject: "Sending Email using javascript",
+//       Body: "Well that was easy!!",
+//   })
+//       .then(function (message) {
+//           alert("mail sent successfully")
+//       });
+// }
