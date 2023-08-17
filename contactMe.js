b9051b3e154db5ac6d03535d550a4524a0ce888a
aaravdubey <daarav101@gmail.com>
2023-08-18 00:03:33 +0530

    small ui improvements & contact me logic

--- contactMe.js
+++ contactMe.js
@@ -1,15 +1,27 @@
-// function handleSubmit() {
-//   alert('hhh')
-//   Email.send({
-//       Host: "smtp.gmail.com",
-//       Username: "daarav101@gmail.com",
-//       Password: "aarav30112002",
-//       To: 'daarav101@gmail.com',
-//       From: "daarav101@gmail.com",
-//       Subject: "Sending Email using javascript",
-//       Body: "Well that was easy!!",
-//   })
-//       .then(function (message) {
-//           alert("mail sent successfully")
-//       });
-// }
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+
+  var templateParams = {
+    from_name: formData.get("name"),
+    email_id: formData.get("email"),
+    message: formData.get("message")
+  };
+  // console.log(templateParams);
+
+  emailjs.send('service_jpkb7zh', 'template_lq0oqr8', templateParams)
+    .then(function (response) {
+      document.getElementById('success-msg').style.display = 'block';
+      document.getElementById('error-msg').style.display = 'none';
+      console.log('SUCCESS!', response.status, response.text);
+      
+    }, function (error) {
+      document.getElementById('success-msg').style.display = 'none';
+      document.getElementById('error-msg').style.display = 'block';
+      console.log('FAILED...', error);
+    });
+
+}
+
+
